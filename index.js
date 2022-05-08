5bcd37effefcad862d827373beae7c876434a821
Arman-Kabir <armankabir8@gmail.com>
2022-05-08 18:34:23 +0600

    add new item api done

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const cors = require("cors");
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// verify jwt token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+    });
+    console.log('inside verifyJWT ', authHeader);
+    next();
+}
+
+
 
 // Database Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hbo9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -19,26 +41,51 @@ async function run() {
     try {
         await client.connect();
         const bikeCollection = client.db("royalBikes").collection("bike");
+        const myitemCollection = client.db("royalBikes").collection("myitem");
+        // console.log(bikeCollection, myitemCollection);
 
-        // get bikes
+        // auth api
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+
+        // get bikes api
         app.get('/inventory', async (req, res) => {
-            const email = req.query.email;
+
+            // const decodedEmail = req.decoded.email;
+            // auth header
+            // const authHeader = req.headers.authorization;
+            // console.log(authHeader);
+
+            // const email = req.query.email;
             const item = req.query.item;
-             console.log(email,item);
+            console.log(item);
             let query = {};
             let bikes;
-            if(email){
-                query = {email:email}
-            }
-            
-            
-            
-            if(item){
+            // --------------
+            // if(email === decodedEmail){
+
+            // }
+            // else{
+            //     res.status(403).send({message: 'forbidden access'})
+            // }
+
+            // -----------
+            // if (email) {
+            //     query = { email: email }
+            // }
+
+            if (item) {
                 const cursor = bikeCollection.find(query);
-                 bikes = await cursor.limit(6).toArray();
-            }else{
+                bikes = await cursor.limit(6).toArray();
+            } else {
                 const cursor = bikeCollection.find(query);
-                 bikes = await cursor.toArray();
+                bikes = await cursor.toArray();
             }
             // console.log(bikes);
             res.send(bikes);
@@ -54,7 +101,7 @@ async function run() {
         });
 
         // add an inventory item
-        app.post('/inventory',async(req,res)=>{
+        app.post('/inventory', async (req, res) => {
             const newService = req.body;
             console.log(newService);
             const result = await bikeCollection.insertOne(newService);
@@ -87,6 +134,52 @@ async function run() {
             console.log(result);
             res.send(result);
         })
+
+        // get new inventory *my item api
+        app.get('/inventoryitem', async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            let query; 
+            let myitem;
+            if(email){
+                query = { email: email };
+            }else{
+                query={};
+            }
+            const cursor = myitemCollection.find(query);
+            myitem = await cursor.toArray();
+            res.send(myitem);
+        })
+
+        // add new inventory *my item api
+        app.post('/inventoryitem', async (req, res) => {
+            const newItem = req.body;
+            console.log(newItem);
+            const result = await myitemCollection.insertOne(newItem);
+            res.send(result);
+        })
+
+         // Delete an inventory item
+         app.delete('/inventoryitem/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await myitemCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // get a single my item**
+        app.get('/inventoryitem/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const bike = await bikeCollection.findOne(query);
+            // console.log(bike)
+            res.send(bike);
+        });
+
+        
+
+        
 
 
 
